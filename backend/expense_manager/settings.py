0d077a6a6702a8a2f41884b7ccede3758e1d48a7
Charumathi <nagineni.charumathi5@gmail.com>
2026-06-15 04:08:34 +0530

    config: update Django settings for production deployment
    - Set ALLOWED_HOSTS to allow all hosts for cloud deployment
    - Add STATIC_ROOT configuration for static file collection

--- backend/expense_manager/settings.py
+++ backend/expense_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r6(*3ai(09gd7)@34!ly_rlg!k7#ghgwtce$2#vej6v^@8ame3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -135,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
